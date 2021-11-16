19077812e7aa21cdce6b99d269ef769b03299cc8
roditee <roditee@naver.com>
2021-11-16 22:24:01 +0900

    211116 goorm level

--- src/goorm/minesweeper.java
+++ src/goorm/minesweeper.java
@@ -1,7 +1,33 @@
 package goorm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class minesweeper {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int m = br.read() - 48;
+		br.read();
+		int n = br.read() - 48;
+		br.read();
 		
+		char[][] board = new char[n][m];
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				board[i][j] = (char)br.read();
+			}
+			br.read();
+		}
+		
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				int cnt = 0;
+				if (board[i][j] != '*') {
+					if (i>0 && board[i-1][j] == '*') cnt++;
+
+				}
+			}
+		}
 	}
 }
